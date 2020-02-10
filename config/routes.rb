c22fb2facdaeaa07982757f2d5ff040b56282984
VinhDT <vinhdt@teky.edu.vn>
2020-02-10 14:27:03 +0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :user do
-    get 'login' => 'sessions/new'
-    post 'login' => 'sessions/create'
+    get 'login' => 'sessions#new'
+    post 'login' => 'sessions#create'
     delete 'logout'  => 'sessions#destroy'
     resources :users
     resources :op_students, only: [:index, :new]
