126e803cfd07d923a681a887077b831bf163249c
VinhDT <vinhdt@teky.edu.vn>
2020-02-11 10:56:02 +0700

    Update account_role checking functions

--- app/controllers/user/home_controller.rb
+++ app/controllers/user/home_controller.rb
@@ -1,12 +1,16 @@
 module User
   class HomeController < ApplicationController
     def dashboard
-      # TODO : Redirect URL based on User role
-      if current_user.is_student?
-        redirect_to user_my_class_path
+      if current_user.nil?
+        redirect_to user_login_path
       else
-        redirect_to user_parent_selection_path
+        if current_user.is_student?
+          redirect_to user_my_class_path
+        else
+          redirect_to user_parent_selection_path
+        end
       end
+      # TODO : Redirect URL based on User role
     end
   end
 end
