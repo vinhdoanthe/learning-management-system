374e7082c33db9a273f0e42bf56a3b818db65229
doantuancanh94 <doantuancanh94@gmail.com>
2020-02-18 18:12:48 +0700

    teaching schedule table

--- app/assets/javascripts/custom_js/opteacherjs.js
+++ app/assets/javascripts/custom_js/opteacherjs.js
@@ -55,19 +55,6 @@ $(document).ready(function(){
 		call_api(data);
 	});
 
-	// Teaching schedule
-	$('.teaching_schedule').click(function(e){
-		e.preventDefault();
-		$(this).parent().find($('.schedule_detail')).css('display','block')
-	})
-
-	$(document).mouseup(function(e) {
-	    var container = $(".schedule_detail");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) 
-	    {
-	        container.hide();
-	    }
-	});
 
 	// Teacher class detail
 	function change_time(str_time){
@@ -149,5 +136,90 @@ $(document).ready(function(){
 	})
 
 	fill_data(lesson_data);
+
+	//Teaching schedule calendar
+	//// TEST
+	function get_date_month(fullDate){
+	  twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
+	  str_date = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+	  return str_date;
+	}
+
+	function set_date_filter(idate) {
+	  idate.setDate(idate.getDate() - idate.getDay() + 1);
+	  begin_week = get_date_month(idate);
+
+	  idate.setDate(idate.getDate() + 6)
+	  end_week = get_date_month(idate);
+	  
+	  $('.start_time').html(begin_week);
+	  $('.end_time').html(end_week);
+
+	  for(var i = 0; i <= 6; i++) {
+			var a = new Date(save_date.getTime());
+			a.setDate(a.getDate() - a.getDay() + i + 1);
+			day = get_date_month(a);
+			if (i != 6){
+				$('.thu_' + (i + 2).toString()).html('Thứ ' + (i + 2).toString() +'<br/><span>'+ day +'</span>')
+			}else{
+				$('.thu_' + (i + 2).toString()).html('Chủ nhật<br/><span>'+ day +'</span>')
+			}
+		}
+	}
+
+	var save_date = new Date();
+	var used_date = new Date();
+	used_date.setDate(used_date.getDate() - used_date.getDay());
+	set_date_filter(used_date);
+
+	function get_teaching_schedule(date){
+		$.ajax({
+			method: 'post',
+			url: '/user/teaching_schedule',
+			data: {'date': date},
+			success: function(res){
+				$('.schedule_info').remove();
+				$.each(res.schedules, function(key, schedule){
+						for(var i = 0; i <= 6; i++){
+							if(schedule[i.toString()]){
+								info = schedule[i.toString()]
+								html = '<td class="bg-eaeaea schedule_info"><a data-placement="top" class="ct-detail ctd-cs3 schedule_link" tabindex="0" role="button" data-html="true" data-toggle="popover" data-trigger="focus" data-content="<ul><li><span>' + info.start_time + ' - ' + info.end_time + ' | ' + info.day + '</span></li><li><strong>Học viện: </strong>' + info.company + '</li><li><strong>Môn học: </strong>' + info.subject + ' - Level ' + info.level +' </li><li><strong>Khoá học: </strong> ' + info.batch + '</li><li><strong>Lớp: </strong> ' + info.course + '</li><li><strong>Số buổi: </strong> ' + info.lesson + '</li></ul>"><span></span>'
+								if(info.state == 'cancel'){
+									html += '<img src="assets/global/images/close.png" alt="">' + info.name + ' </a></td>'
+								}else{
+									html += info.name + ' </a></td>'
+								}
+							}else{
+								html = '<td class="bg-fff schedule_info"></td>'
+							}
+
+							$('.' + key).append(html);
+							$('.schedule_link').click(function(e){ 
+								$(this).popover('show');
+							})
+						}
+				})
+			},
+			error: function(res){
+				console.log(res);
+			}
+		})
+	}
+
+	get_teaching_schedule(save_date);
+
+	$('.schedule_time_previous').click(function(e){
+		save_date.setDate(save_date.getDate() - 7)
+		used_date = new Date(save_date.getTime());
+		set_date_filter(used_date);
+		get_teaching_schedule(save_date);
+	})
+
+	$('.schedule_time_next').click(function(e){
+		save_date.setDate(save_date.getDate() + 7)
+		used_date = new Date(save_date.getTime());
+		set_date_filter(used_date);
+		get_teaching_schedule(save_date);
+	})
 })
 
--- app/services/user/op_teachers_service.rb
+++ app/services/user/op_teachers_service.rb
@@ -32,14 +32,14 @@ class User::OpTeachersService
       all_students.merge!(student_info)
     end
 
-    if session.state == 'confirm'
+    if session.state == Learning::Constant::Batch::Session::STATE_CONFIRM
       session_students = session.op_session_students
       session_students.each do |st|
         student = st.op_student_course.op_student
         student_info = {student.id => {:note => st.note, :attendance => '', :status => st.op_student_course.state, :code => student.code, :name => student.full_name}}
         students.merge!(student_info)
       end
-    elsif session.state == 'done'
+    elsif session.state == Learning::Constant::Batch::Session::STATE_DONE
       session_students = session.op_attendance_lines
       session_students.each do |st|
         note = st.note_1
@@ -55,5 +55,50 @@ class User::OpTeachersService
     end
 
     all_students
+  end
+
+  def self.teching_schedule session_list, params
+    if params[:date].present?
+      sessions = session_list.where(:start_datetime => params[:date].to_time.all_week)
+    else
+      sessions = session_list.where(:start_datetime => Time.now.all_week)
+    end
+
+    schedule_hash = {'s1' => {}, 's2' => {}, 'c1' => {}, 'c2' => {}, 't1' => {}, 't2' => {}}
+
+    sessions.each do |session|
+      time = session.end_datetime.localtime
+      name = session.name
+      start_time = session.start_datetime.localtime.strftime('%H:%M')
+      end_time = time.strftime('%H:%M')
+      day = time.strftime('%Y-%m-%d')
+      company = Common::ResCompany.find(session.company_id).name
+      subject = session.op_subject.name
+      level = session.op_subject.level.to_s
+      batch = session.op_batch.name
+      course = session.op_batch.op_course.code
+      lesson = '5/12'
+      status = session.state
+
+      session_info = { name: name, start_time: start_time, end_time: end_time, day: day, company: company, subject: subject, level: level, batch: batch, course: course, lesson: lesson, status: status}
+      record = { time.wday => session_info}
+
+      case time.hour.to_i
+      when 8..10
+        schedule_hash['s1'].merge!(record)
+      when 10..12
+        schedule_hash['s2'].merge!(record)
+      when 12..14
+        schedule_hash['c1'].merge!(record)
+      when 14..16
+        schedule_hash['c2'].merge!(record)
+      when 16..18
+        schedule_hash['t1'].merge!(record)
+      when 18.20
+        schedule_hash['t2'].merge!(record)
+      end
+    end
+
+    schedule_hash
   end
 end
