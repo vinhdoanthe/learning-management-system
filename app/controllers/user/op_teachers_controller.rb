feb62ce47bddce468600be346e912caa29a777af
doantuancanh94 <doantuancanh94@gmail.com>
2020-02-19 18:08:06 +0700

    teacher class detail

--- app/controllers/user/op_teachers_controller.rb
+++ app/controllers/user/op_teachers_controller.rb
@@ -37,8 +37,17 @@ module User
     def teacher_class_detail
       @batch = Learning::Batch::OpBatch.find(params[:batch_id].to_i)
       @sessions = @batch.op_sessions.order(start_datetime: 'ASC')
+      if params[:subject_id].present?
+        @current_subject_id = params[:subject_id].to_i
+        @sessions = @sessions.where(:subject_id => params[:subject_id].to_i)
+      else
+        current_subject = @sessions.where.not('state = ? OR state = ?', Learning::Constant::Batch::Session::STATE_DONE, Learning::Constant::Batch::Session::STATE_CANCEL).first
+        current_subject = @sessions.last if current_subject.blank?
+        @current_subject_id = current_subject.subject_id
+        @sessions = @sessions.where(subject_id: @current_subject_id)
+      end
 
-      if params[:session_index]
+      if params[:session_index].present?
         @session = @sessions[params[:session_index].to_i]
       else
         @session = @sessions.where('start_datetime >= ?', Time.now).first
@@ -47,11 +56,12 @@ module User
       @session = @sessions.last unless @session
       @session_index = @sessions.index(@session)
       @subject = @session.op_subject
+      sessions_time = @sessions.pluck(:start_datetime, :end_datetime)
 
       all_students = OpTeachersService.new.teacher_class_detail @batch, @session
 
       if request.method == 'POST'
-        render json: {batch: @batch, session: @session, session_index: @session_index, subject: @subject, note: @note, students: all_students}
+        render json: {batch: @batch, session: @session, session_index: @session_index, subject: @subject, note: @note, students: all_students, sessions_time: sessions_time}
       end
     end
 
