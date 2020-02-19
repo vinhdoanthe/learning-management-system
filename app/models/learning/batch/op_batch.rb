2fccd6b6723dfe5bc63f6cc65996915a955a6307
VinhDT <vinhdt@teky.edu.vn>
2020-02-19 09:42:07 +0700

    Check current session, level of batch

--- app/models/learning/batch/op_batch.rb
+++ app/models/learning/batch/op_batch.rb
@@ -10,11 +10,24 @@ module Learning
 
       has_many :op_sessions, class_name: 'Learning::Batch::OpSession', foreign_key: 'batch_id'
       has_many :op_session_students, class_name: 'Learning::Batch::OpSessionStudent', foreign_key: 'batch_id'
-      has_many :op_faculties, -> { distinct }, :class_name => 'User::OpFaculty', through: :op_sessions
-      
+      has_many :op_faculties, -> {distinct}, :class_name => 'User::OpFaculty', through: :op_sessions
+
       # TO DO batch status
       def check_status
         self.active
+      end
+
+      def current_session_level
+        last_done_session = OpSession.where(batch_id: self.id, state: Learning::Constant::Batch::Session::STATE_DONE).last
+        if last_done_session.nil?
+          'Chưa có buổi học nào diễn ra'
+        else
+          if last_done_session.op_subject.nil?
+            last_done_session.count.to_s + 'Chưa có dữ liệu Subject'
+          else
+            last_done_session.count.to_s + ' - Level ' + last_done_session.op_subject.level.to_s
+          end
+        end
       end
     end
   end
