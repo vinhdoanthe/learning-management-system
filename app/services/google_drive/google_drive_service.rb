0cbc8c4d91316e583e5abcfe3212fbff650bf08f
VinhDT <vinhdt@teky.edu.vn>
2020-02-24 14:02:11 +0700

    Update google drive api

--- app/services/google_drive/google_drive_service.rb
+++ app/services/google_drive/google_drive_service.rb
@@ -15,40 +15,51 @@ module Learning
     TOKEN_PATH = "token.yaml".freeze
     SCOPE = Google::Apis::DriveV3::AUTH_DRIVE_METADATA_READONLY
 
-    def initialize
-      client_id = Google::Auth::ClientId.from_file("#{Rails.root}/config/google_drive_client_secret.json")
-      scope = ['https://www.googleapis.com/auth/drive']
-      token_store = Google::Auth::Stores::RedisTokenStore.new(redis: $redis)
-      @authorizer = Google::Auth::UserAuthorizer.new(
-          client_id, scope, token_store)
-      user_id = 'darazone'
-      @credentials = @authorizer.get_credentials(user_id)
-      @drive = Google::Apis::DriveV2::DriveService.new
-      @drive.authorization = @credentials
+    def authorize
+      client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
+      token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
+      authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
+      user_id = "default"
+      credentials = authorizer.get_credentials user_id
+      if credentials.nil?
+        url = authorizer.get_authorization_url base_url: OOB_URI
+        puts "Open the following URL in the browser and enter the " \
+         "resulting code after authorization:\n" + url
+        code = gets
+        credentials = authorizer.get_and_store_credentials_from_code(
+            user_id: user_id, code: code, base_url: OOB_URI
+        )
+      end
+      credentials
     end
 
-    def authorization_url
-      @authorizer.get_authorization_url(base_url: OOB_URI)
-    end
+    # Initialize the API
+    drive_service = Google::Apis::DriveV3::DriveService.new
+    drive_service.client_options.application_name = APPLICATION_NAME
+    drive_service.authorization = authorize
 
-    def get_credentials
-      @credentials
-    end
-
-    def set_authorization
-      @drive.authorization = @credentials
-    end
-
-    def get_drive
-      @drive
-    end
-
-    def save_credentials(code)
-      user_id = 'darazone'
-      @credentials = @authorizer.get_and_store_credentials_from_code(
-          user_id: user_id, code: code, base_url: OOB_URI)
-      @drive.authorization = @credentials
-    end
+    # def authorization_url
+    #   @authorizer.get_authorization_url(base_url: OOB_URI)
+    # end
+    #
+    # def get_credentials
+    #   @credentials
+    # end
+    #
+    # def set_authorization
+    #   @drive.authorization = @credentials
+    # end
+    #
+    # def get_drive
+    #   @drive
+    # end
+    #
+    # def save_credentials(code)
+    #   user_id = 'darazone'
+    #   @credentials = @authorizer.get_and_store_credentials_from_code(
+    #       user_id: user_id, code: code, base_url: OOB_URI)
+    #   @drive.authorization = @credentials
+    # end
 
     # def list_files(options = {})
     #   @drive.list_files(options: options)
