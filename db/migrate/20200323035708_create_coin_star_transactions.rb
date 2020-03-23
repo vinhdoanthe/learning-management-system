66d40abe8699d11d6f62fb7e78371eeea3608d4a
VinhDT <vinhdt@teky.edu.vn>
2020-03-23 14:03:55 +0700

    Cập nhật import slides

--- db/migrate/20200323035708_create_coin_star_transactions.rb
+++ db/migrate/20200323035708_create_coin_star_transactions.rb
@@ -1,9 +1,9 @@
 class CreateCoinStarTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :coin_star_transactions do |t|
-      t.int :give_to
-      t.int :give_by
-      t.int :activity_id
+      t.integer :give_to
+      t.integer :give_by
+      t.integer :activity_id
       t.string :activity_type, limit: 30
 
       t.timestamps
