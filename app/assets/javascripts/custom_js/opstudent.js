dd0d5b04fe7a89e406f77d80e9ec4458faf3951d
doantuancanh94 <doantuancanh94@gmail.com>
2020-03-28 20:50:11 +0700

    lỗi submit bài tập - thêm loading layer

--- app/assets/javascripts/custom_js/opstudent.js
+++ app/assets/javascripts/custom_js/opstudent.js
@@ -49,6 +49,67 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 $('document').ready(function () {
+  var waitingDialog = waitingDialog || (function ($) {
+    'use strict';
+
+	// Creating modal dialog's DOM
+	var $dialog = $(
+		'<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style=" overflow-y:visible;">' +
+		'<div class="modal-dialog modal-m">' +
+		'<div class="modal-content">' +
+			'<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
+			'<div class="modal-body">' +
+				'<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+			'</div>' +
+		'</div></div></div>');
+
+	return {
+		/**
+		 * Opens our dialog
+		 * @param message Custom message
+		 * @param options Custom options:
+		 * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
+		 * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
+		 */
+		show: function (message, options) {
+			// Assigning defaults
+			if (typeof options === 'undefined') {
+				options = {};
+			}
+			if (typeof message === 'undefined') {
+				message = 'Loading';
+			}
+			var settings = $.extend({
+				dialogSize: 'm',
+				progressType: '',
+				onHide: null // This callback runs after the dialog was hidden
+			}, options);
+
+			// Configuring dialog
+			$dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
+			$dialog.find('.progress-bar').attr('class', 'progress-bar');
+			if (settings.progressType) {
+				$dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
+			}
+			$dialog.find('h3').text(message);
+			// Adding callbacks
+			if (typeof settings.onHide === 'function') {
+				$dialog.off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
+					settings.onHide.call($dialog);
+				});
+			}
+			// Opening dialog
+			$dialog.modal();
+		},
+		/**
+		 * Closes dialog
+		 */
+		hide: function () {
+			$dialog.modal('hide');
+		}
+	};
+
+})(jQuery);
     $('.student_evaluate_view').on('click', function () {
         sudent_id = $(this).data('student');
         session_id = $(this).data('session');
@@ -121,6 +182,7 @@ $('document').ready(function () {
         })
 
         $('#answer_the_question').on('click', function(){
+          waitingDialog.show();
             question_choices = $("input[name='student_choise_answer']:checked").map(function(){
               return $(this).val();
             }).get()
@@ -138,7 +200,10 @@ $('document').ready(function () {
                 method: 'POST',
                 url: '/learning/answer_question',
                 data: { session_id: session_id, question: question, question_choices: question_choices, user_question: user_question},
-                dataType: 'script'
+                dataType: 'script',
+              success: function(){
+          waitingDialog.hide();
+              }
             })
         })
     }
