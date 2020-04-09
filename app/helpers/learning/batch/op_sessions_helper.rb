92c45143967b2aa21f5916b81f7d823a079e2614
VinhDT <vinhdt@teky.edu.vn>
2020-04-09 15:09:17 +0700

    Update logic lay lession tu session Fix mot vai loi UI nho

--- app/helpers/learning/batch/op_sessions_helper.rb
+++ app/helpers/learning/batch/op_sessions_helper.rb
@@ -3,7 +3,20 @@ module Learning
     module OpSessionsHelper
       def attendance_line(session_id, student_id)
         Learning::Batch::OpAttendanceLine.where(session_id: session_id, student_id: student_id).last
-      end        
+      end
+
+      def get_lesson_of_session op_session
+        if op_session.lession_id.nil?
+          att_sheet = op_session.op_attendance_sheets.last
+          if att_sheet.nil?
+            nil
+          else
+            Learning::Course::OpLession.where(id: att_sheet.lession_id).first
+          end
+        else
+          op_session.op_lession
+        end
+      end 
     end
   end
 end
