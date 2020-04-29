a034be32d65a55d2180aad1c23ca9e7fa5bf634a
VinhDT <vinhdt@teky.edu.vn>
2020-04-29 11:06:29 +0700

    Change Module name AnswerMark

--- app/models/learning/homework/answer_mark.rb
+++ app/models/learning/homework/answer_mark.rb
@@ -1,5 +1,5 @@
 module Learning
-  module LearningRecord
+  module Homework
     class AnswerMark < ApplicationRecord
       belongs_to :user_answer
       belongs_to :user, class_name: 'User::Account::User', foreign_key: 'teacher_id'
