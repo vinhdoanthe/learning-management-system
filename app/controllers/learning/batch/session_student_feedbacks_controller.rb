d0a53d98556d1cda975c8367af08009a95230d1a
doantuancanh94 <doantuancanh94@gmail.com>
2020-05-12 16:11:57 +0700

    add noti photopost | create student feedback

--- app/controllers/learning/batch/session_student_feedbacks_controller.rb
+++ app/controllers/learning/batch/session_student_feedbacks_controller.rb
@@ -1,2 +1,9 @@
 class Learning::Batch::SessionStudentFeedbacksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def student_feedback
+    result = Learning::Batch::SessionStudentFeedbacksService.new.create_new_feedback params, current_user
+    
+    render json: result
+  end
 end
