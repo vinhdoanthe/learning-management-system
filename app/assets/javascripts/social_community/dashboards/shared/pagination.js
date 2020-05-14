2d66df61964e4c46168228f4b1101f89c7003e1e
VinhDT <vinhdt@teky.edu.vn>
2020-05-14 16:49:05 +0700

    Move get_feeds from dashboard to pagination JS

--- app/assets/javascripts/social_community/dashboards/shared/pagination.js
+++ app/assets/javascripts/social_community/dashboards/shared/pagination.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+	get_feeds();
+  
   more_posts_url = $('.pagination .next_page a').attr('href');
   // Load more posts if page is not scrollable
   if (more_posts_url && ($('#homeFeeds').height() < $(window).height())) {
@@ -11,6 +13,15 @@ $(document).ready(function () {
   })
 });
 
+
+function get_feeds() {
+  console.log('Start load feeds')
+  $.ajax({
+    method: 'GET',
+    url: '/social_community/home_feeds',
+    dataType: 'script'
+  })
+}
 
 function loadMorePosts() {
   more_posts_url = $('.pagination .next_page a').attr('href');
--- app/assets/javascripts/social_community/dashboards/student/dashboard.js
+++ app/assets/javascripts/social_community/dashboards/student/dashboard.js
@@ -4,29 +4,20 @@
 
 // On page loaded: call to get feed content
 $(document).ready(function(){
-	get_feeds();
-  get_feed_content();
+  // get_feed_content();
 	get_noti_content();
 	get_new_user();
   get_attendance_report();
 })
 
-function get_feeds() {
-  console.log('Start load feeds')
-  $.ajax({
-    method: 'GET',
-    url: '/social_community/home_feeds',
-    dataType: 'script'
-  })
-}
 
 // Catch events
 function get_feed_content(){
-	$.ajax({
-		method: 'GET',
-		url: '/social_community/albums_with_comments',
-		dataType: 'script'
-	})
+	// $.ajax({
+	// 	method: 'GET',
+	// 	url: '/social_community/albums_with_comments',
+	// 	dataType: 'script'
+	// })
 
 	$.ajax({
 		method: 'GET',
