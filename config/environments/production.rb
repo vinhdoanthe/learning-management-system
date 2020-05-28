1ab05ac6c2615ea6dff24653d91ec8c7c0f67d0c
VinhDT <vinhdt@teky.edu.vn>
2020-05-28 17:41:11 +0700

    Update setting gmail for sending email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      user_name: ENV['gmail_username'],
-      password: ENV['gmail_password'],
+      user_name: Settings.action_mailer['gmail_username'],
+      password: Settings.action_mailer['gmail_password'],
       authentication: 'plain',
       enable_starttls_auto: true
   }
