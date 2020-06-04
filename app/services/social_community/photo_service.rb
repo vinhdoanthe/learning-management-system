d894c3fa128ad66f99562820d6437e2ea628eba1
VinhDT <vinhdt@teky.edu.vn>
2020-06-04 14:55:22 +0700

    Refactor code writing process image before attach to photo

--- app/services/social_community/photo_service.rb
+++ app/services/social_community/photo_service.rb
@@ -36,37 +36,7 @@ class SocialCommunity::PhotoService
 
   def create_new_photo session_id, album_id, image, user, post
     # Process image
-    processed_image = MiniMagick::Image.new(image.tempfile.path)
-    processed_image.format 'jpg'
-    w,h = processed_image.dimensions
-    max_w = 1920
-    max_h = 1080
-    if w > max_w or h > max_h
-      if (w/max_w) < (h/max_h)
-        new_h = max_h
-        new_w = w*new_h/h
-      else
-        new_w = max_w
-        new_h = h*new_w/w
-      end
-      new_dimensions="#{new_w}x#{new_h}" 
-      processed_image.resize(new_dimensions)
-    end
-    processed_image.write processed_image.path
-    # Optimize image
-    image_optim = ImageOptim.new(:pack => true,
-                                 :pngout => false,
-                                 :advpng => false, 
-                                 :nice => 20, 
-                                 :allow_lossy => true,
-                                 :jpegrecompress => false,
-                                 :jpegtran => false,
-                                 :jpegoptim => {
-                                   :allow_lossy => true,
-                                   :strip => :all,
-                                   :max_quality => 85
-                                 })
-    image_optim.optimize_image!(processed_image.path)
+    processed_image = pre_process_image image
     photo = SocialCommunity::Photo.new
     photo.album_id = album_id
     photo.session_id = session_id
@@ -100,6 +70,47 @@ class SocialCommunity::PhotoService
       photo.delete
       post.delete
       return { type: 'success', message: 'Xoá ảnh thành công' }
+    end
+  end
+
+
+  private
+  def pre_process_image image
+    begin
+      processed_image = MiniMagick::Image.new(image.tempfile.path)
+      processed_image.format 'jpg'
+      w,h = processed_image.dimensions
+      max_w = 1920
+      max_h = 1080
+      if w > max_w or h > max_h
+        if (w/max_w) < (h/max_h)
+          new_h = max_h
+          new_w = w*new_h/h
+        else
+          new_w = max_w
+          new_h = h*new_w/w
+        end
+        new_dimensions="#{new_w}x#{new_h}" 
+        processed_image.resize(new_dimensions)
+      end
+      processed_image.write processed_image.path
+      # Optimize image
+      image_optim = ImageOptim.new(:pack => true,
+                                   :pngout => false,
+                                   :advpng => false, 
+                                   :nice => 20, 
+                                   :allow_lossy => true,
+                                   :jpegrecompress => false,
+                                   :jpegtran => false,
+                                   :jpegoptim => {
+                                     :allow_lossy => true,
+                                     :strip => :all,
+                                     :max_quality => 85
+                                   })
+      image_optim.optimize_image!(processed_image.path)
+      processed_image
+    rescue => exception
+      puts exception.backtrace
     end
   end
 end
