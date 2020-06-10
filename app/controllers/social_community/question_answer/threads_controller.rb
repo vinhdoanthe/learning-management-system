b9a0bfccc2f903265e4f146167b20bc1c82ef9ca
VinhDT <vinhdt@teky.edu.vn>
2020-06-10 17:38:27 +0700

    Update new static front-end

--- app/controllers/social_community/question_answer/threads_controller.rb
+++ app/controllers/social_community/question_answer/threads_controller.rb
@@ -2,6 +2,16 @@ module SocialCommunity::QuestionAnswer
   class ThreadsController < ApplicationController
     def create
       new_thread = SocialCommunity::QuestionAnswer::ThreadsService.create_thread(permit_params)
+      if new_thread.nil?
+         
+      else
+        respond_to do |format|
+          format.html
+          format.js {
+
+          }
+        end
+      end
     end
 
     def update
