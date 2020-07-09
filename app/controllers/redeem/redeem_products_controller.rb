2ef7079bae1f57848e10f6f3e582e267204fd1d7
ntsc279@gmail.com <thangnc@teky.edu.vn>
2020-07-09 17:21:52 +0700

    update Doi qua lay diem

--- app/controllers/redeem/redeem_products_controller.rb
+++ app/controllers/redeem/redeem_products_controller.rb
@@ -1,13 +1,14 @@
 class Redeem::RedeemProductsController < ApplicationController
+  
   def index
     
-    @page_title = t('adm.redeem.redeem_points')
-
+    @page_title 	 = t('adm.redeem.redeem_points')
     @redeem_products = Redeem::RedeemProduct.select('id,name,brand, available_color,available_size,description,price,category ').all
 
   end
 
   def show
-    @redeem_product = Redeem::RedeemProduct.where(id: params[:id]).first
+  	@page_title 	 = t('sidebar.redeem')
+    @redeem_product  = Redeem::RedeemProduct.where(id: params[:id]).first
   end
 end
