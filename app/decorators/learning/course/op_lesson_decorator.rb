db27ab5c9d2d48b48f9ef9dbe7e998bf221e1dd2
VinhDT <vinhdt@teky.edu.vn>
2020-07-18 20:28:02 +0700

    Fix mot so loi UI/UX phan lop hoc

--- app/decorators/learning/course/op_lesson_decorator.rb
+++ app/decorators/learning/course/op_lesson_decorator.rb
@@ -6,4 +6,12 @@ class Learning::Course::OpLessonDecorator < SimpleDelegator
       "#{I18n.t('learning.lesson')} #{lession_number}"
     end
   end
+
+  def display_lesson_full_name_with_subject
+    if !op_subject.nil?
+      "#{I18n.t('learning.subject').upcase} #{op_subject.level}: #{name}"
+    else
+      "#{name}"
+    end
+  end
 end
