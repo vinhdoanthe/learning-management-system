5bff0a5a32ecfef0cad827035628bcaea51b2885
VinhDT <vinhdt@teky.edu.vn>
2020-07-28 13:53:35 +0700

    Add show (controller, view) session_student_feedback

--- app/controllers/learning/batch/session_student_feedbacks_controller.rb
+++ app/controllers/learning/batch/session_student_feedbacks_controller.rb
@@ -1,9 +1,30 @@
 class Learning::Batch::SessionStudentFeedbacksController < ApplicationController
   skip_before_action :verify_authenticity_token
-
+  before_action :authorize!, :only => [:show]
   def student_feedback
     result = Learning::Batch::SessionStudentFeedbacksService.new.create_new_feedback params, current_user
-    
+
     render json: result
+  end
+
+  def show
+
+  end
+
+  private
+  def get_feedback
+    if params[:id].present?
+      @feedback = Learning::Batch::SessionStudentFeedback.where(id: params[:id].to_i).first
+    else
+      @feedback = nil
+    end
+  end
+
+  def authorize!
+    get_feedback
+    if !Learning::Batch::SessionStudentFeedbacksService.can_authorize?(@feedback, current_user) 
+      flash[:danger] = I18n.t('flash_notice.no_permissions')
+      redirect_to root_path
+    end
   end
 end
--- app/models/learning/batch/session_student_feedback.rb
+++ app/models/learning/batch/session_student_feedback.rb
@@ -3,15 +3,15 @@ class Learning::Batch::SessionStudentFeedback < ApplicationRecord
   
   belongs_to :user, class_name: 'User::Account::User', foreign_key: 'feedback_by'
   belongs_to :op_session, class_name: 'Learning::Batch::OpSession', foreign_key: 'session_id'
+  belongs_to :feeling_type, class_name: 'Common::FeelingType', foreign_key: 'feeling_type_id'
 
   acts_as_notifiable :users,
     targets: ->(feedback, key) {
     (Learning::Batch::SessionStudentFeedbacksService.get_subscribed_users feedback.session_id).uniq
-  },
-  notifiable_path: :feedback_path
+  }
 
-  def feedback_path
-    session_student_feedback_path(id)
+  def notifiable_path post, key
+    learning_feedback_path(id)
   end
 
   def create_notifications
