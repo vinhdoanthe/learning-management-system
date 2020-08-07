61be1724cc2c5f414f622c88514d0fee93bffe10
VinhDT <vinhdt@teky.edu.vn>
2020-08-07 18:26:33 +0700

    Update: add more complication to Question model

--- app/models/learning/material/question.rb
+++ app/models/learning/material/question.rb
@@ -14,13 +14,30 @@ module Learning
 
       validates :question_type, presence: true
       validates :question, presence: true
-      validate :has_question_choice
+      validate :valid_question_choice
 
-      def has_question_choice
-        if question_type != 'text' && question_choices.length == 0
-          errors.add(:question_choice, 'Thiếu lựa chọn trả lời')
+      def valid_question_choice
+        if question_type != 'text' 
+          if question_choices.length == 0
+            errors.add(:question_choice, 'Thiếu lựa chọn trả lời')
+          else
+            count_right_choice = 0
+            question_choices.each do |question_choice|
+              if question_choice.is_right_choice
+                count_right_choice += 1
+              end
+            end
+            if (count_right_choice == 0)
+              errors.add(:question_choice, 'Chưa khai báo lựa chọn đúng')
+            else
+              if question_type == 'single' && (count_right_choice > 1)
+                errors.add(:question_choice, 'Câu hỏi Single Choice không thể có nhiều hơn 1 lựa chọn đúng')
+              end
+            end
+          end
         end
       end
+
     end
   end
 end
--- app/models/learning/material/question_choice.rb
+++ app/models/learning/material/question_choice.rb
@@ -2,7 +2,8 @@ module Learning
   module Material
     class QuestionChoice < ApplicationRecord
       belongs_to :question, inverse_of: :question_choices
-
+      
+      validates :choice_content, presence: true
     end
   end
 end
