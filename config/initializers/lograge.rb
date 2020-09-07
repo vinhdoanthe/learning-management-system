86b3b69303c4255fe5e4f4caec6bc881b989bde5
VinhDT <vinhdt@teky.edu.vn>
2020-09-07 15:22:43 +0700

    Update lograge config

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,12 +1,21 @@
 require 'lograge/sql/extension'
 
 Rails.application.configure do
-    # Lograge config
-    config.lograge.enabled = true
-    config.lograge.formatter = Lograge::Formatters::Json.new
-    config.colorize_logging = false
+  # Lograge config
+  config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Json.new
+  config.colorize_logging = false
 
-    config.lograge.custom_options = lambda do |event|
-      { :params => event.payload[:params] }
+  config.lograge.ignore_actions = ['ActiveStorage::BlobsController#show', 'ActiveStorage::RepresentationsController#show', 'ActiveStorage::DiskController#show', 'ActiveStorage::DiskController#update', 'ActiveStorage::DirectUploadsController#create']
+
+  config.lograge.custom_payload do |controller|
+    {
+      host: controller.request.host,
+      user_id: controller.current_user.try(:id)
+    }
+  end
+
+  config.lograge.custom_options = lambda do |event|
+    { :params => event.payload[:params] }
   end
 end
