a8535f3bb01f49bfb29b0a7a58eb88ef82f4f4ad
Teky Academy <tekyacademy@Tekys-iMac.local>
2020-09-19 09:04:04 +0700

    update UI cancel session

--- app/decorators/learning/batch/op_session_decorator.rb
+++ app/decorators/learning/batch/op_session_decorator.rb
@@ -19,15 +19,15 @@ class Learning::Batch::OpSessionDecorator < SimpleDelegator
       if Time.now > (start_datetime - 15.mins)
         "<button type='button' class='btn btn-default btn-embossed' id='teacher_checkin' data-toggle='modal' data-target='#modal_checkin' ><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button>"
       else
-        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Chưa tới giờ điểm danh'><button type='button' class='btn btn-default btn-embossed'  disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
+        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Chưa tới giờ Check-in'><button type='button' class='btn btn-default btn-embossed'  disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
       end
     else
       if check_in_state == 'none'
-        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Quên điểm danh'><button type='button' class='btn btn-danger'  disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
+        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Quên Check-in'><button type='button' class='btn btn-danger'  disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
       elsif check_in_state == 'good'
-        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Điểm danh đúng giờ'><button type='button' class='btn btn-success'  disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
+        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Check-in đúng giờ'><button type='button' class='btn btn-success'  disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
       elsif check_in_state == 'late'
-        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Điểm danh muộn'><button type='button' class='btn btn-warning' disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
+        "<div class='tooltip-span' data-toggle='tooltip' data-html='true' title='Check-in muộn'><button type='button' class='btn btn-warning' disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button></div>"
       else
         "<button type='button' class='btn btn-default btn-embossed'  disabled><span>#{ image_tag(ActionController::Base.helpers.asset_path('hotel-2.png')) }</span>Check-in</button>"
       end
