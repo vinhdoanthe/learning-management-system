8d564bbc0f8daf3c0c8091f43f7cf1ede24648eb
Teky Academy <tekyacademy@Tekys-iMac.local>
2020-09-30 16:00:45 +0700

    adm homework

--- app/controllers/adm/learning/activity_controller.rb
+++ app/controllers/adm/learning/activity_controller.rb
@@ -14,15 +14,12 @@ class Adm::Learning::ActivityController < Adm::AdmController
 
   # Quan ly Bai tap ve nha
   def homework
-    # binding.pry
     @report_title_page = t('adm.learning_activity_management_homework')
     @param_form = Learning::Homework::UserAnswerService.form_parameter(params, request)
     # Danh sach
-    @list_user_answers = Learning::Homework::UserAnswerService.get_user_answers_type_question_text(@param_form)
+    @count_right, @count_waiting, @count_wrong, @list_user_answers = Learning::Homework::UserAnswerService.get_user_answers_type_question_text(@param_form)
     # batch_params = {'company_id': 1,'start_date': @param_form[:from_date].to_s,'end_date': @param_form[:to_date].to_s,'active': true}
     #@list_batch = Learning::Batch::OpBatchService.find_batch_by_params(batch_params)
     #puts @list_batch
-    # binding.pry
-    return
   end
 end
