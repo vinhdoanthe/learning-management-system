ea67c77399b784c460c70b1ec370c8632e19c49b
Teky Academy <tekyacademy@Tekys-iMac.local>
2020-10-01 11:43:54 +0700

    fix css adm session

--- app/services/adm/learning/sessions_service.rb
+++ app/services/adm/learning/sessions_service.rb
@@ -41,12 +41,21 @@ class Adm::Learning::SessionsService
     end
 
     if params[:start_time].present?
-      sessions = Learning::Batch::OpSession.includes(:op_batch, :res_company, :photos, :op_lession, :op_faculty, :op_attendance_lines).where(query).where(start_datetime: (params[:start_time].to_datetime..params[:end_time].to_datetime)).distinct.order(start_datetime: :DESC).limit(25).offset(offset).pluck(:id, :state, :start_datetime, :end_datetime, 'op_batch.code', 'op_batch.id', 'op_batch.company_id', 'res_company.name', 'op_lession.name', 'op_lession.id', 'op_session.count', 'op_lession.lession_number', 'op_faculty.full_name', 'op_faculty.id')
+      sessions = Learning::Batch::OpSession.includes({ op_batch: :op_course }, :res_company, :photos, :op_lession, :op_faculty, :op_attendance_lines).where(query).where(start_datetime: (params[:start_time].to_datetime..params[:end_time].to_datetime)).distinct.order(start_datetime: :DESC).limit(25).offset(offset).pluck(:id, :state, :start_datetime, :end_datetime, 'op_batch.code', 'op_batch.id', 'op_batch.company_id', 'res_company.name', 'op_lession.name', 'op_lession.id', 'op_session.count', 'op_lession.lession_number', 'op_faculty.full_name', 'op_faculty.id', 'op_course.name')
     else
-      sessions = Learning::Batch::OpSession.includes(:op_batch, :res_company , :photos, :op_lession, :op_faculty, :op_attendance_lines).where(query).where(start_datetime: Time.at(0)..Time.now).distinct.order(start_datetime: :DESC).limit(25).offset(offset).pluck(:id, :state, :start_datetime, :end_datetime, 'op_batch.code', 'op_batch.id', 'op_batch.company_id', 'res_company.name', 'op_lession.name', 'op_lession.id', 'op_session.count', 'op_lession.lession_number', 'op_faculty.full_name', 'op_faculty.id')
+      sessions = Learning::Batch::OpSession.includes({ op_batch: :op_course }, :res_company , :photos, :op_lession, :op_faculty, :op_attendance_lines).where(query).where(start_datetime: Time.at(0)..Time.now).distinct.order(start_datetime: :DESC).limit(25).offset(offset).pluck(:id, :state, :start_datetime, :end_datetime, 'op_batch.code', 'op_batch.id', 'op_batch.company_id', 'res_company.name', 'op_lession.name', 'op_lession.id', 'op_session.count', 'op_lession.lession_number', 'op_faculty.full_name', 'op_faculty.id', 'op_course.name')
+    end
+    #sessions = Learning::Batch::OpSession.includes({ op_batch: :op_course }, :res_company , :photos, :op_lession, :op_faculty, :op_attendance_lines).where(query).where(start_datetime: Time.at(0)..Time.now).distinct.order(start_datetime: :DESC).group(:id, 'op_batch.code', 'op_batch.id', 'res_company.name', 'op_lession.name', 'op_lession.id', 'op_faculty.full_name', 'op_faculty.id', 'op_course.name').limit(25).offset(offset).pluck(:id, :state, :start_datetime, :end_datetime, 'op_batch.code', 'op_batch.id', 'op_batch.company_id', 'res_company.name', 'op_lession.name', 'op_lession.id', 'op_session.count', 'op_lession.lession_number', 'op_faculty.full_name', 'op_faculty.id', 'op_course.name', 'count(op_attendance_line.id)')
+
+    sessions.map!{ |info| { id: info[0], state: info[1], start_datetime: info[2], end_datetime: info[3], batch_id: info[5], batch_code: info[4], company_id: info[6], company_name: info[7], lesson_name: info[8], lesson_id: info[9], session_count: info[10], lesson_number: info[11], faculty_name: info[12], faculty_id: info[13], course_name: info[14] } }
+
+    sessions.each do |session|
+      count_attendance_line = Learning::Batch::OpAttendanceLine.where(session_id: session[:id]).count
+      count_session_student = Learning::Batch::OpSessionStudent.where(session_id: session[:id]).count
+      session.merge!({ count_attendance_line: count_attendance_line, count_session_student: count_session_student})
     end
 
-    sessions.map!{ |info| { id: info[0], state: info[1], start_datetime: info[2], end_datetime: info[3], batch_id: info[5], batch_code: info[4], company_id: info[6], company_name: info[7], lesson_name: info[8], lesson_id: info[9], session_count: info[10], lesson_number: info[11], faculty_name: info[12], faculty_id: info[13] } }
+    sessions
   end
 
   def get_allow_user_companies user
