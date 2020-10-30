35c37251ad053e77df80e5d47a7f3a7769e746c9
VinhDT <vinhdt@teky.edu.vn>
2020-10-30 17:08:05 +0700

    Add fb_social_count function

--- app/services/contest/contests_service.rb
+++ app/services/contest/contests_service.rb
@@ -1,4 +1,7 @@
 class Contest::ContestsService
+  require 'erb'
+  require 'net/http'
+
   def leader_board contest_id
     #student_name, topic, c_project_id, res_company => region, week???????????????/
     contest = Contest::Contest.where(id: contest_id).first
@@ -17,21 +20,21 @@ class Contest::ContestsService
              Time.now - 1.week
            end
 
-      w_projects =  contest.contest_projects
-                    .joins(user: { op_student: :res_company })
-                    .joins(:student_project)
-                    .joins(:project_criterions)
-                    .where(created_at: time.beginning_of_week..time.end_of_week)
-                    .order(score: :DESC)
-                    .select('distinct(tk_contest_projects.id)',
-                            :created_at, :user_id,
-                            'op_student.full_name as student_name',
-                            'sc_student_projects.name as project_name',
-                            :views,
-                            :score,
-                            'res_company.name as company_name',
-                            :project_id)
-                    .limit(6)
+    w_projects =  contest.contest_projects
+      .joins(user: { op_student: :res_company })
+      .joins(:student_project)
+      .joins(:project_criterions)
+      .where(created_at: time.beginning_of_week..time.end_of_week)
+      .order(score: :DESC)
+      .select('distinct(tk_contest_projects.id)',
+              :created_at, :user_id,
+              'op_student.full_name as student_name',
+              'sc_student_projects.name as project_name',
+              :views,
+              :score,
+              'res_company.name as company_name',
+              :project_id)
+      .limit(6)
 
     project_ids = w_projects.pluck(:project_id)
     w_project_imgs = {}
@@ -47,8 +50,8 @@ class Contest::ContestsService
 
   def awarded_projects contest, type
     awarded_projects = contest.contest_projects
-                        .joins(:contest_prize)
-                        .where(tk_contest_prizes: { prize: '1', prize_type: type })
+      .joins(:contest_prize)
+      .where(tk_contest_prizes: { prize: '1', prize_type: type })
 
     details = {}
     awarded_projects.each do |project|
@@ -65,10 +68,34 @@ class Contest::ContestsService
     user = c_project.user
     user_avatar = user.avatar&.thumbnail
     project_img = project.image
-    
+
     details = c_project.as_json
     details.merge! ({ 'user_avatar' => user_avatar, 'project_name' => project.name, 'project_img' => project_img, 'created_at' => c_project.created_at, 'student_name' => project.op_student&.full_name, 'like' => like, 'share' => share, 'views' => c_project.views })
 
     details
+  end
+
+  def update_fb_social_count
+    # get active projects
+    # TODO update how to get active projects
+    active_projects = Contest::ContestProject.all
+
+    # call FB graph API and update for each project
+    active_projects.each do |project|
+      plain_url = "https://lms.teky.online/contest/contest_projects/show/#{project.id}"
+      encoded_url = ERB::Util.url_encode(plain_url)
+      url = URI("https://graph.facebook.com/v8.0/?id=#{encoded_url}&fields=engagement&access_token=#{Settings.fbapp_access_token}")
+
+      https = Net::HTTP.new(url.host, url.port)
+      https.use_ssl = true
+
+      request = Net::HTTP::Get.new(url)
+      request["Content-Type"] = "application/x-www-form-urlencoded"
+      request.body = "access_token=#{Settings.fbapp_access_token}"
+
+      response = https.request(request)
+      puts response.read_body
+      # TODO: update project here...
+    end
   end
 end
