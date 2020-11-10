035fbe54e95951553151ee754761baf6f41da7ff
VinhDT <vinhdt@teky.edu.vn>
2020-11-10 13:52:22 +0700

    Update: fix bugs Teky Contest

--- app/assets/javascripts/contests.js
+++ app/assets/javascripts/contests.js
@@ -1,7 +1,6 @@
-//=require jquery
 //=require jquery_ujs
-//= require jquery.remotipart
+//=require jquery.remotipart
 //=require bootstrap
-//= require activestorage
+//=require activestorage
 //=require_tree ./contest/js
 //=require "html2canvas/dist/html2canvas.js"
