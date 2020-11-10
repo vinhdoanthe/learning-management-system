a0eea93b2d7b33ac923833b5bf49eb52d352e09b
VinhDT <vinhdt@teky.edu.vn>
2020-11-10 14:31:15 +0700

    Update contests.js

--- app/assets/javascripts/contests.js
+++ app/assets/javascripts/contests.js
@@ -1,15 +1,13 @@
 //=require contest/js/jquery-3.4.1.min.js
 //=require contest/js/jquery-migrate-1.4.1.js
+//=require jquery.remotipart
 //=require contest/js/popper.min.js
 //=require contest/js/bootstrap.min.js
 //=require contest/js/owl.carousel.min.js
 //=require contest/js/owl.carousel2.thumbs.js
 //=require contest/js/jquery.matchHeight-min.js
-//=require contest/js/datepicker.min.js
-//=require contest/js/locales/datepicker.vi-VN.js
 //=require contest/js/function.js
 //=require contest/js/style.js
 
-//=require jquery.remotipart
 //=require activestorage
 //=require html2canvas/dist/html2canvas.js
