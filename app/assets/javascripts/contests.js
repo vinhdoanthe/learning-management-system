769579fc2cdb4008a552ad0a7d96811752e12560
VinhDT <vinhdt@teky.edu.vn>
2020-11-10 14:04:08 +0700

    Update assets.rb

--- app/assets/javascripts/contests.js
+++ app/assets/javascripts/contests.js
@@ -1,6 +1,7 @@
+//=require jquery
 //=require jquery_ujs
 //=require jquery.remotipart
 //=require bootstrap
 //=require activestorage
 //=require_tree ./contest/js
-//=require "html2canvas/dist/html2canvas.js"
+//=require html2canvas/dist/html2canvas.js
