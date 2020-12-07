c6cf8a0fdba47c1c0fb6f77afe0c0174f3adcb2e
VinhDT <vinhdt@teky.edu.vn>
2020-12-07 09:53:21 +0700

    Update Sentry raven dsn

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,12 @@ module TEKYLMS
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :vi
     config.active_storage.replace_on_assign_to_many = false
+    ActiveRecord::SessionStore::Session.primary_key = 'session_id'
     config.time_zone = 'Asia/Ho_Chi_Minh'
     config.filter_parameters << :password
     Raven.configure do |config|
-      config.dsn = 'https://cd0a4f0f5c2347879badd2965e6ba3df:972441f1488b4f97a035b7b180d63416@o398229.ingest.sentry.io/5253599'
-      config.environments = %w[ production ]
+      config.dsn = Settings.sentry_raven_dsn
+      # config.environments = %w[ production ]
       config.excluded_exceptions += ['ActiveStorage::FileNotFoundError']
     end
   end
